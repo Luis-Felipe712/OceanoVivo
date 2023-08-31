88ba15bd8adcfd6bdcbbcd5f3578b86ede28836e
EC2 Default User <ec2-user@ip-172-31-45-37.ec2.internal>
2023-08-31 00:17:08 +0000

    SegundaVesao

---
+++ index.js
@@ -0,0 +1,56 @@
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }s
+}
+
+
+window.onmousedown = e => handleOnDown(e);
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+window.onmouseup = e => handleOnUp(e);
+window.ontouchend = e => handleOnUp(e.touches[0]);
+window.onmousemove = e => handleOnMove(e);
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+const toggleCheckbox = document.querySelector(".toggle-checkbox");
+const htmlElement = document.querySelector("html");
+
+toggleCheckbox.addEventListener("change", function() {
+  if (this.checked) {
+    htmlElement.classList.add("theme-dark");
+    htmlElement.classList.remove("theme-light");
+  } else {
+    htmlElement.classList.add("theme-light");
+    htmlElement.classList.remove("theme-dark");
+  }
+});
+
+
+
+
